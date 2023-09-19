d5b42a84e4950a9cd761e2dfc0cf520dbe6b83b4
Ramazan HABER <ramazanhaberr@gmail.com>
2023-09-19 23:44:05 +0300

    aa

--- DapperNew/Ogrenciler.cs
+++ DapperNew/Ogrenciler.cs
@@ -1,10 +1,4 @@
 ﻿using Dapper.Contrib.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace DapperNew
 {
     [Table("Ogrenciler")]
@@ -12,11 +6,7 @@ namespace DapperNew
     {
         [Key]
         public int id { get; set; }
-
         public string ad { get; set; }
-
         public int yas { get; set; }
-
     }
-
 }
